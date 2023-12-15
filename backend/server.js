723164245fc6e269b0fd3c2ddfe44c4cca923175
Rahul <146400573+Rahul2767@users.noreply.github.com>
2023-12-15 19:47:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,17 +23,8 @@ app.use(cookieParser())
 
 app.use('/api/users', userRoutes)
 
-if (process.env.NODE_ENV === "production") {
-    const __dirname = path.resolve()
-    app.use(express.static(path.join(__dirname, '../../ecommerce-app/build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'ecommerce-app', 'build', 'index.html'))
-    })
-} else {
-    app.get('/', (req, res) => {
-        res.send('welcome to server')
-    })
+app.get('/', (req, res) => {
+    res.send('welcome to server')
 }
 
 
