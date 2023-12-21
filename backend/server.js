e0e055345516e1442e526ae9da6f136e15175567
Rahul Dhiman <2021mca015@timt.ac.in>
2023-12-21 16:59:26 +0530

    'change urls and render array '

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB()
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: 'https://fakeflipkart.onrender.com',
+    origin: ['https://fakeflipkart.onrender.com', 'http://localhost:3000'],
 }))
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
