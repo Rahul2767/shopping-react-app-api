2c709ac58090e68c039d814b4857411880e5d11f
Rahul Dhiman <2021mca015@timt.ac.in>
2023-12-21 17:16:26 +0530

    allow headers

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://fakeflipkart.onrender.com');
     res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
