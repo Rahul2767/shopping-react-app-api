ac4870d44d4eb0d5853bd4c4590e62e80cdab096
Rahul Dhiman <2021mca015@timt.ac.in>
2023-12-22 11:41:14 +0530

    hgmnfgdbf

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB()
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://fakeflipkart.onrender.com');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
