c4ecdcc906f60c3c6f1cc048479fa24257742941
Rahul Dhiman <2021mca015@timt.ac.in>
2023-12-22 10:52:13 +0530

    222

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,14 @@ import userRoutes from './routes/userRoutes.js'
 import { notfound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import cookieParser from 'cookie-parser'
+const data = require('../data.json')
 
 connectDB()
 
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://fakeflipkart.onrender.com');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
@@ -25,11 +26,13 @@ app.use(cookieParser())
 
 app.use('/api/users', userRoutes)
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.send('welcome to server')
 })
 
-
+app.get('/products', (req, res) => {
+    res.json(data)
+})
 
 app.use(notfound)
 app.use(errorHandler)
