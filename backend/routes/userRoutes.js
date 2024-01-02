731bdad6c8b481ca3b499047aa51c42ae5fb03f8
Rahul Dhiman <2021mca015@timt.ac.in>
2024-01-02 10:31:35 +0530

    updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,24 @@
-import express from 'express'
+import express from "express";
 const router = express.Router();
-import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, createOrder, getOrders } from '../controller/userController.js'
-import { protect } from '../middleware/authMiddleware.js';
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  createOrder,
+  getOrders,
+} from "../controller/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
-router.post('/', registerUser)
-router.post('/auth', authUser)
-router.post('/logout', logoutUser)
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-router.post('/order', protect, createOrder)
-router.post('/getOrders', protect, getOrders)
+router.post("/", registerUser);
+router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router.post("/order", protect, createOrder);
+router.post("/getOrders", protect, getOrders);
 
-export default router
+export default router;
