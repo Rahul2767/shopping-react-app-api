c138f2181b2ea8213e6bc874c32cbf95cd373cc6
Rahul Dhiman <2021mca015@timt.ac.in>
2024-01-08 10:18:13 +0530

    Fix localhost

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,48 @@
-import express, { urlencoded } from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-const PORT = process.env.PORT || 5000
-import userRoutes from './routes/userRoutes.js'
-import { notfound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
-import cookieParser from 'cookie-parser'
-import data from './data.json' assert {type: 'json'};
+import express, { urlencoded } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+import userRoutes from "./routes/userRoutes.js";
+import { notfound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import data from "./data.json" assert { type: "json" };
 
-connectDB()
+connectDB();
 
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://fakeflipkart.onrender.com');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://fakeflipkart.onrender.com"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.use(express.json())
-app.use(urlencoded({ extended: true }))
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use('/api/users', userRoutes)
+app.use("/api/users", userRoutes);
 
-app.get('/', (req, res) => {
-    res.send('welcome to server')
-})
+app.get("/", (req, res) => {
+  res.send("welcome to server");
+});
 
-app.get('/products', (req, res) => {
-    res.json(data)
-})
+app.get("/products", (req, res) => {
+  res.json(data);
+});
 
-app.use(notfound)
-app.use(errorHandler)
+app.use(notfound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log('server is running on port ' + PORT)
-})
+  console.log("server is running on port " + PORT);
+});
